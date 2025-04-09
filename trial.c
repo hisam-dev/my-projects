5d1bace961b144b8e13a0369a5f3ce7550f095e5
hisam-dev <muhammedhaisam616@gmail.com>
2025-04-09 00:29:38 -0700

    Update trial.c

--- trial.c
+++ trial.c
@@ -2,7 +2,7 @@
 void main()
 {
   int a,b,diff;
-printf("Enter the two numbres:");
+printf("Enter two numbres:");
 scanf("%d%d",&a,&b);
 diff=a-b;
 printf("The difference of two numbers is  %d",diff);
